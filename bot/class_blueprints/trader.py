82ac6d9e0675a438dab064ab65a63d0c3c0c6cc4
Michiel <mvtklooster@hotmail.com>
2021-10-11 15:58:28 +0200

    Fix bug

--- bot/class_blueprints/trader.py
+++ bot/class_blueprints/trader.py
@@ -124,7 +124,7 @@ class TraderAPI:
             "timestamp": ms_time,
         }
 
-        query_string = f"symbol={symbol}&orderId={order_id}&timestamp={ms_time}"
+        query_string = f"symbol={symbol.upper()}&orderId={order_id}&timestamp={ms_time}"
         signature = hmac.new(self.secret.encode("utf-8"), query_string.encode("utf-8"), hashlib.sha256).hexdigest()
         params["signature"] = signature
         return requests.delete(self.endpoint + request, params=params, headers=self.header)
